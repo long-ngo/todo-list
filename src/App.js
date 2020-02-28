f5d207f7d179cc1ec0a1def277d47d4e32268d49
hi <ngothanhlongnoyssiw@gmail.com>
2020-02-28 17:40:57 +0700

    things to do

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.todoItem = [
+      { title:'Đi đá bóng', isComplete: false }, 
+      { title:'Đi đổ xăng', isComplete: false }, 
+      { title:'Đi chơi', isComplete: false }
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.todoItem.length > 0 && this.todoItem.map((item, index) => 
+          <TodoItem key={index} item={item} />)
+        }
+        { this.todoItem.length === 0 && 'Nothing here' }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './TodoItem.css';
+
+class TodoItem extends React.Component {
+  render() {
+    const {item} = this.props;
+    let className = 'TodoItem';
+    if (item.isComplete) {
+      className += ' TodoItem-complete';
+    }
+    return (
+      <div className={className}>
+        <p>{item.title}</p>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
+
